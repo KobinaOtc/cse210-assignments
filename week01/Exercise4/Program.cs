db25151339712b72c046a98f3c9be4d52e560e31
Kobina Otchere <kobinaotchere@Kobinas-MacBook-Pro.local>
2025-05-08 13:57:45 +0000

    List activie core done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        bool active = true;
+        int sum = 0;
+        int largest = 0;
+        float average = 0;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        while (active)
+        {
+            Console.Write("Enter number: ");
+            string numTxt = Console.ReadLine();
+            int num = int.Parse(numTxt);
+
+            if (num == 0)
+            {
+                foreach (int i in numbers)
+                {
+                    sum += i;
+                    if (i > largest)
+                    {
+                        largest = i;
+                    }
+                }
+                average = sum / numbers.Count;
+
+                Console.WriteLine($"The sum is: {sum}");
+                Console.WriteLine($"The average is: {average}");
+                Console.WriteLine($"The largest is: {largest}");
+
+                active = false;
+            }
+            else
+            {
+                numbers.Add(num);
+            }
+
+        }
     }
 }
