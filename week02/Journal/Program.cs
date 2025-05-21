c67e90f5b974407b12d6a8e5341290570bd4091d
Kobina Otchere <kobinaotchere@Kobinas-MacBook-Pro.local>
2025-05-21 16:50:26 +0000

    added prompt generator

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -6,24 +6,7 @@ class Program
     {
         bool active = true;
         Entries entries = new Entries();
-        List<string> prompts = new List<string>();
-        prompts.Add("If you had one thing you could do over today, what would it be and why?");
-        prompts.Add("What emotion have you felt the most this week, and what triggered it?");
-        prompts.Add("What’s one thing you wish more people understood about you?");
-        prompts.Add("Describe a moment recently when you felt truly alive. What made it special?");
-        prompts.Add("What’s something you’re avoiding, and what’s the real reason behind it?");
-        prompts.Add("If your inner child could speak to you today, what would they say?");
-        prompts.Add("What does “peace” look and feel like to you right now?");
-        prompts.Add("Who or what are you most grateful for today, and why?");
-        prompts.Add("What’s one limiting belief you’re ready to let go of?");
-        prompts.Add("What would your ideal day look like from morning to night?");
-        prompts.Add("What would your ideal day look like from morning to night?");
-        prompts.Add("What’s a lesson you’ve learned the hard way, and how has it shaped you?");
-        prompts.Add("If fear didn’t exist, what would you do differently?");
-        prompts.Add("Write a letter to your future self five years from now. What do you hope they’ve learned or become?");
-        prompts.Add("What does success mean to you—personally, not by society’s standards?");
-        prompts.Add("What’s a small joy you experienced today that you don’t want to forget?");
-        Random randomGenerator = new Random();
+        PromptGen prompt = new PromptGen();
 
         while (active)
         {
@@ -37,13 +20,13 @@ class Program
             string ans = Console.ReadLine();
             DateTime currentDateTime = DateTime.Now;
             string dataString = currentDateTime.ToShortDateString();
-            int index = randomGenerator.Next(0, 14);
+            // int index = randomGenerator.Next(0, 14);
 
             if (ans == "1")
             {
                 Entry entry = new Entry();
                 entry._date = dataString;
-                entry._prompt = prompts[index];
+                entry._prompt = prompt.PromptUser();
                 Console.WriteLine(entry._prompt);
                 entry._response = Console.ReadLine();
                 entries._entries.Add(entry);
---
+++ week02/Journal/PromptGen.cs
@@ -0,0 +1,29 @@
+
+public class PromptGen
+{
+    public string PromptUser()
+    {
+        List<string> prompts =
+        [
+            "If you had one thing you could do over today, what would it be and why?",
+            "What emotion have you felt the most this week, and what triggered it?",
+            "What’s one thing you wish more people understood about you?",
+            "Describe a moment recently when you felt truly alive. What made it special?",
+            "What’s something you’re avoiding, and what’s the real reason behind it?",
+            "If your inner child could speak to you today, what would they say?",
+            "What does “peace” look and feel like to you right now?",
+            "Who or what are you most grateful for today, and why?",
+            "What’s one limiting belief you’re ready to let go of?",
+            "What would your ideal day look like from morning to night?",
+            "What would your ideal day look like from morning to night?",
+            "What’s a lesson you’ve learned the hard way, and how has it shaped you?",
+            "If fear didn’t exist, what would you do differently?",
+            "Write a letter to your future self five years from now. What do you hope they’ve learned or become?",
+            "What does success mean to you—personally, not by society’s standards?",
+            "What’s a small joy you experienced today that you don’t want to forget?",
+        ];
+        Random randomGenerator = new Random();
+        int index = randomGenerator.Next(0, 14);
+        return prompts[index];
+    }
+}
