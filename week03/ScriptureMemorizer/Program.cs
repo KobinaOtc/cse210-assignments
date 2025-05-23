18f75e8a790ea6c19bb0a277698e77ee5b888485
Kobina Otchere <kobinaotchere@Kobinas-MacBook-Pro.local>
2025-05-23 12:57:59 +0000

    feature added

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        int enterCount = 0;
+        string quit = "";
+        Scripture scripture = new Scripture();
+        scripture.SetEdit();
+        scripture.Display();
+        while (enterCount < 7 && quit != "quit")
+        {
+            string input = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                scripture.Display();
+                enterCount++;
+            }
+            else
+            {
+                quit = input;
+            }
+        }
     }
 }
