4e2d64b21fcc2642123457c17b8f5af8c591f10c
Kobina Otchere <kobinaotchere@Kobinas-MacBook-Pro.local>
2025-05-30 22:59:15 +0000

    Classes defined

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,30 @@
+/*
+-------------------------------------
+|             Comment               |
+-------------------------------------
+| - _commenterName: string          |
+| - _text: string                   |
+-------------------------------------
+| + Comment(commenterName: string, text: string) |
+| + DisplayComment(): void          |
+-------------------------------------
+*/
+class Comment
+{
+    public string _commenterName;
+    public string _text;
+
+    // Consturctor
+    Comment(string commenterName, string text)
+    {
+        _commenterName = commenterName;
+        _text = text;
+    }
+
+    // Methods
+    public void DisplayComment()
+    {
+        Console.WriteLine($"Name: {_commenterName}");
+        Console.WriteLine($"Comment: {_text}");
+    }
+}
